8649b4a6f73c0b6e7fc9e21fe191bc146ed1301d
carlos <kyokushin26>
2020-06-11 16:20:19 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Basics React</h1>
+      <h2>INIT !!!</h2>
+    </div>
+  );
+}
+
+export default App;
